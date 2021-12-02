a6e2e05ecb6bf69500ae8fa72314ba7ca05edf18
fawazbayureksa <fawwazbayureksa@gmail.com>
2021-12-02 20:57:34 +0800

    update Authcontrolller commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Costumers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
@@ -28,9 +30,20 @@ class AuthController extends Controller
                 error: ['Pengguna tidak ditemukan'],
             );
         } else {
-            if(Hash::check($pass,$data->password) ) //cek jika password cocok
+            if (Hash::check($pass, $data->password)) //cek jika password cocok
             {
+                $data->token = hash('sha256', Str::random(10)); //token untuk dikirim ke client
+                unset($data->password); //hilangkan informasi password yang dikirm ke client
+                $data->update(); //update token disimpan ke table costumer
 
+                return $this->out(data: $data, status: 'Ok');
+            } else //jika password tidak cocok
+            {
+                return $this->out(
+                    status: 'Gagal',
+                    code: 401, //401 unathorized
+                    error: ['Login gagal'],
+                );
             }
         }
     }
