0d78ddcbe1ce64fbcc486595a49dc9826cc1b0fe
fawazbayureksa <fawwazbayureksa@gmail.com>
2021-12-03 22:27:46 +0800

    Update commits

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Orders;
+use Illuminate\Http\Request;
+use App\Models\Products;
+use Carbon\Carbon;
+
+class OrderController extends BaseController
+{
+    public function store(){
+        $products = Products::find(\request('product_id'));
+        if ($products == null) //jika produk tidak ditemukan
+        {
+            return $this->out(status:'Gagal',code:404,error:'Produk tidak ditemukan');
+        }
+        $order = new Orders();
+        $order->order_date = Carbon::now('Asia/Makassar');
+        $order->product_id = $products->id;
+        $order->costumers_id = request('costumer_id');
+        $order->qty= request('qty');
+        $order->price= request('price');
+
+        if($order->save() == true) //jika berhasil menyimpan
+        {
+            return $this->out(data:$order,status:'Ok',code:201);
+        }else{
+            return $this->out(status:'gagal',error:['Order gagal di simpan'],code:504);
+        }
+
+    }
+}
