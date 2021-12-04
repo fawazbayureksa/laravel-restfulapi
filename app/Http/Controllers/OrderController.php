836d6c26ee0c2d08bda5bdb749ee1649fe01de24
fawazbayureksa <fawwazbayureksa@gmail.com>
2021-12-04 11:43:55 +0800

    Update commits

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Orders;
 use Illuminate\Http\Request;
 use App\Models\Products;
@@ -8,25 +9,25 @@ use Carbon\Carbon;
 
 class OrderController extends BaseController
 {
-    public function store(){
+    public function store()
+    {
         $products = Products::find(\request('product_id'));
         if ($products == null) //jika produk tidak ditemukan
         {
-            return $this->out(status:'Gagal',code:404,error:'Produk tidak ditemukan');
+            return $this->out(status: 'Gagal', code: 404, error: 'Produk tidak ditemukan');
         }
         $order = new Orders();
         $order->order_date = Carbon::now('Asia/Makassar');
         $order->product_id = $products->id;
         $order->costumer_id = request('costumer_id');
-        $order->qty= request('qty');
-        $order->price= request('price');
+        $order->qty = request('qty');
+        $order->price = $products->price;
 
-        if($order->save() == true) //jika berhasil menyimpan
+        if ($order->save() == true) //jika berhasil menyimpan
         {
-            return $this->out(data:$order,status:'Ok',code:201);
-        }else{
-            return $this->out(status:'gagal',error:['Order gagal di simpan'],code:504);
+            return $this->out(data: $order, status: 'Ok', code: 201);
+        } else {
+            return $this->out(status: 'gagal', error: ['Order gagal di simpan'], code: 504);
         }
-
     }
 }
