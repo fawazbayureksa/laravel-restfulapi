3958f7c02227aef20d53dbaf278a67ed5d34169f
fawazbayureksa <fawwazbayureksa@gmail.com>
2021-12-07 23:08:02 +0800

    upadte login

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -10,8 +10,9 @@
 <body>
     @yield('konten')
 
+    <h1>Hello world</h1>
 
-    
+
 
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="{{ url('assets/js/bootstrap.bundle.min.js',[]) }}"></script>
