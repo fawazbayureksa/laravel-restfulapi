08812c593ff87f559c83cfda7fb50fcefc6d313c
fawazbayureksa <fawwazbayureksa@gmail.com>
2021-12-07 23:22:16 +0800

    update form login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,12 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-</head>
-<body>
 
-</body>
-</html>
+
+@extends('template')
+@section('title','Form Login')
+@section('konten')
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-body">
+                        <h3>Form Login</h3>
+                        <div class="mb-3">
+                            <label for="">Email</label>
+                            <input type="email" placeholder="Masukkan email ..." name="email" id="email" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="">Password</label>
+                            <input type="password" placeholder="Masukkan password ..." name="password" id="password" class="form-control">
+                        </div>
+                        <div class="">
+                            <button class="btn btn-primary btn-sm float-end" id="btn-login">Masuk</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
