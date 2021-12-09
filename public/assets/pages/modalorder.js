08375a3a65fd892413ca8070402875c64f2b28e3
fawazbayureksa <fawwazbayureksa@gmail.com>
2021-12-09 23:54:38 +0800

    update

--- public/assets/pages/modalorder.js
+++ public/assets/pages/modalorder.js
@@ -30,7 +30,7 @@ function fillProduct(){
             var content = '';
             for(var i=0;i<data.length; i++){
                 var item = data[i];
-                content+= `<option value='${item.id}'>${item.title}</option>`
+                content+= `<option value='${item.id}'>${item.title} | ${item.category}</option>`
             }
             $('select[name=product_id]').html(content);
         },
@@ -41,15 +41,35 @@ function fillProduct(){
     });
 }
 
-function save(){
-
+function save(id){
+    $.ajax({
+        url:'api/orders' + (id !== undefined ? `/${id}`:''),
+        method:id !== undefined ? 'PATCH':'POST',
+        dataType:'json',
+        data:{
+            'product_id' : $('select[name=product_id]').val(),
+            'costumer_id' :$('select[name=costumer_id]').val(),
+            'qty' : $('input[name=qty]').val(),
+        },
+        headers: { 'token' : window.localStorage['token']},
+        success:(res)=>{
+            console.log('sukses',res);
+            alert('Data order berhasil di tambahkan');
+            $('#exampleModal').modal('hide');
+            refreshData();
+        },
+        error: (res,status, err)=>{
+            console.log('error:',res);
+            alert('Order gagal ditambahkan ')
+        }
+    });
 }
 
 document.addEventListener("DOMContentLoaded", (c) => {
     fillProduct();
     fillCustumer();
 
-    $('button#simpan'),on('click',(e)=>{
+    $('button#simpan').on('click',(e)=>{
         save();
     });
 });
